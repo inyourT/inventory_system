99c295013a497c513d3f62f7aeecb7b928dfdda0
inyourT <gagunatulis@gmail.com>
2025-10-02 21:24:48 +0700

    Update: Endpoint Product POST

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -31,3 +31,41 @@ exports.getOneProduct = async (req, res) => {
 };
 
 // create product
+exports.createProduct = async (req,res) => {
+    try {
+
+        const { name, sku, stock, price, supplierId, categoryId } = req.body;
+
+        if(!name) {
+            return res.status(400).json({message: "Product name is required"})
+        }
+
+        if(!sku) {
+            return res.status(400).json({message: "SKU is required"})
+        }
+
+        if(!price) {
+            return res.status(400).json({message: "Price is required"})
+        }
+
+
+        const product = await prisma.product.create({
+            data: {
+                name,
+                sku,
+                stock: stock || 0,
+                price: parseFloat(price),
+                supplierId: supplierId ? parseInt(supplierId): null,
+                categoryId: categoryId ? parseInt(categoryId): null
+            }
+        });
+        res.status(product)
+    }catch (error){
+        console.error(error)
+
+        if(error.code == "P2002") {
+            return res.status(400).json({Message: "SKU must be unique"});
+        }
+        res.status(500).json({message: "Somthing went wrong"})
+    }
+}
