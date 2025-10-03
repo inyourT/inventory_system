b2d79e307168ff85bdae5ec38fe7606a62778f92
inyourT <gagunatulis@gmail.com>
2025-10-03 10:56:34 +0700

    Update Routers Product Finish

--- src/routes/productRouters.js
+++ src/routes/productRouters.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const { getAllProduct,
     getOneProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
 
 } = require ("../controllers/productController");
 const router = express.Router();
 
 router.get("/", getAllProduct);
 router.get("/:id", getOneProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct)
 
 
 module.exports = router;
